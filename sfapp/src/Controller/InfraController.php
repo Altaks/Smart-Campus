1b4bf32f794fd82e32f17b72db845e456214f79b
Kevin Simon <ksimon@etudiant.univ-lr.fr>
2023-11-18 13:54:41 +0100

    fin d'ajout de la foctionnalité de modification d'un système d'acquisition, prêt à la vérification

--- sfapp/src/Controller/InfraController.php
+++ sfapp/src/Controller/InfraController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Batiment;
+use App\Entity\Salle;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -24,25 +28,72 @@ class InfraController extends AbstractController
     }
 
     #[Route('/infra/systemes-acquisition/modifier/{id}', name: 'infra-modifier-systemes-acquisition')]
-    public function modifierSystemesAcquisition(ManagerRegistry $managerRegistry, int $id) : Response
+    public function modifierSystemesAcquisition(ManagerRegistry $managerRegistry, ?int $id, Request $request) : Response
     {
+
         $entityManager = $managerRegistry->getManager();
 
-        $dispositifsRepository = $entityManager->getRepository('App\Entity\SystemeAcquisition');
+        $systemeAcquisitionRepository = $entityManager->getRepository('App\Entity\SystemeAcquisition');
         $batimentRepository = $entityManager->getRepository('App\Entity\Batiment');
         $sallesRepository = $entityManager->getRepository('App\Entity\Salle');
 
-        $dispositif = $dispositifsRepository->find($id);
+        $systemeAcquisition = $systemeAcquisitionRepository->findOneBy(
+            array('id' => $id)
+        );
+
+        if ($request->getMethod() == 'POST') {
+
+            $systemeAcquisition->setAdresseMac($_POST['adresseMac']);
+
+            if ($_POST['salle'] == '-1') {
+                $systemeAcquisition->setSalle(null);
+            } else {
+                $systemeAcquisition->setSalle($sallesRepository->findOneBy(
+                    array('id' => $_POST['salle'])
+                ));
+            }
+
+            $entityManager->flush();
+
+            return $this->redirectToRoute('infra_liste_systemes-acquisition');
+        }
 
         $listeBatiments = $batimentRepository->findAll();
         $listeSalles = $sallesRepository->findAll();
 
         return $this->render('infra/modifier-sa.html.twig', [
             'id' => $id,
-            'dispositf' => $dispositif,
+            'systemeAcquisition' => $systemeAcquisition,
             'listeBatiments' => $listeBatiments,
             'listeSalles' => $listeSalles
         ]);
+    }
+
+    #[Route('/infra/systemes-acquisition/modifier/{id}/valider', name: 'infra-valider-modifier-systemes-acquisition')]
+    public function validerModifierSystemesAcquisition(ManagerRegistry $managerRegistry, int $id) : Response
+    {
+        $entityManager = $managerRegistry->getManager();
+
+        $systemeAcquisitionRepository = $entityManager->getRepository('App\Entity\SystemeAcquisition');
+        $batimentRepository = $entityManager->getRepository('App\Entity\Batiment');
+        $sallesRepository = $entityManager->getRepository('App\Entity\Salle');
+
+        $systemeAcquisition = $systemeAcquisitionRepository->findOneBy(
+            array('id' => $id)
+        );
+
+        $listeBatiments = $batimentRepository->findAll();
+        $listeSalles = $sallesRepository->findAll();
+
+        $systemeAcquisition->setAdresseMac($_POST['adresseMac']);
+        $systemeAcquisition->setSalle($sallesRepository->findOneBy(
+            array('id' => $_POST['salle'])
+        ));
+
+        $entityManager->persist($systemeAcquisition);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('infra_liste_systemes-acquisition');
     }
 
     #[Route('/infra/batiments-et-salles', name: 'app_infra_batiments-et-salles')]
