b08005441d533c8162695ed4f123a3bacaba956c
lonq <luc.lacotte@etudiant.univ-lr.fr>
2023-12-13 14:15:33 +0100

    Création des premiers test de l'US2.20 de la route /infra/technicien/systemes-acquisition

---
+++ webapp/sfapp/tests/Controller/Infra/Technicien/systemeAcquisitionTechnicienTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\tests\Controller\Infra\Technicien;
+
+use App\Repository\UtilisateurRepository;
+use DateInterval;
+use DateTime;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use App\Service\ReleveService;
+
+class systemeAcquisitionTechnicienTest extends WebTestCase
+{
+    public function test_controleur_infra_route_infra_systemes_acquisition_requete_en_tant_que_technicien(): void
+    {
+        $client = static::createClient();
+
+        // retrieve the test user
+        $userRepository = static::getContainer()->get(UtilisateurRepository::class);
+        $testUser = $userRepository->findOneBy(['identifiant' => 'testTechnicien']);
+
+        // simulate $testUser being logged in
+        $client->loginUser($testUser);
+
+        $crawler = $client->request('GET', '/infra/technicien/systemes-acquisition');
+        $this->assertResponseIsSuccessful();
+
+        $this->assertSelectorTextContains('h1', "Liste des systèmes d'acquisition");
+        $h2 = $crawler->filter("h2");
+        $this->assertEquals("Systèmes d'acquisition fonctionnels", $h2->eq(0)->text());
+    }
+
+    public function test_controleur_infra_route_infra_systemes_acquisition_est_fonctionnel()
+    {
+        $client = static::createClient();
+
+        // retrieve the test user
+        $userRepository = static::getContainer()->get(UtilisateurRepository::class);
+        $testUser = $userRepository->findOneBy(['identifiant' => 'testTechnicien']);
+
+        // simulate $testUser being logged in
+        $client->loginUser($testUser);
+
+        $crawler = $client->request('GET', '/infra/technicien/systemes-acquisition');
+        $this->assertResponseIsSuccessful();
+
+        $tr = $crawler->filter('.th-fonctionnel');
+
+        for($i = 0; $i < $tr->count(); $i++) {
+            $tag = $tr->eq($i)->text();
+            $releve = getDernier(intval($tag));
+
+            $dateReleve = new DateTime($releve["date"]);
+            $date = new DateTime('2023-12-13 11:00:00');
+            $dateDiff = $date->diff($dateReleve);
+
+            $this->assertLessThan(6,$dateDiff->days);
+            $this->assertNotEquals(null,$releve["co2"]);
+            $this->assertNotEquals(null,$releve["hum"]);
+            $this->assertNotEquals(null,$releve["temp"]);
+        }
+    }
+}
