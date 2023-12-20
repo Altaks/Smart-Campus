8ff82e96a132215aa0b10ff3aebade9b80f28ced
Luc Lacotte <luc.lacotte@etudiant.univ-lr.fr>
2023-12-20 17:55:47 +0100

    Création test : Selectionner le SA à ajouter dans la salle

--- webapp/sfapp/tests/Controller/PlanExpControllerTest.php
+++ webapp/sfapp/tests/Controller/PlanExpControllerTest.php
@@ -2,6 +2,9 @@
 
 namespace App\tests\Controller;
 
+use App\Entity\SystemeAcquisition;
+use App\Repository\DemandeTravauxRepository;
+use App\Repository\SystemeAcquisitionRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use App\Repository\UtilisateurRepository;
 
@@ -40,5 +43,77 @@ class PlanExpControllerTest extends WebTestCase
 
         $client->request('GET', '/plan');
         $this->assertResponseStatusCodeSame(302, $client->getResponse()->getStatusCode());
+    }
+
+    public function test_demande_travaux_route_connexion_valide(): void
+    {
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UtilisateurRepository::class);
+        $testUser = $userRepository->findOneBy(['identifiant' => 'yghamri']);
+
+        // simulate $testUser being logged in
+        $client->loginUser($testUser);
+
+        $travauxRepository = static ::getContainer()->get(DemandeTravauxRepository::class);
+        $travaux = $travauxRepository->findOne();
+
+        for($i = 0; $i < count($travaux); $i++) {
+            $client->request('GET', '/plan/demande-travaux/'.$travaux[$i]->getId());
+            $this->assertResponseIsSuccessful();
+        }
+    }
+
+    public function test_demande_travaux_route_connexion_invalide_technicien(): void
+    {
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UtilisateurRepository::class);
+        $testUser = $userRepository->findOneBy(['identifiant' => 'jmalki']);
+
+        // simulate $testUser being logged in
+        $client->loginUser($testUser);
+
+        $travauxRepository = static ::getContainer()->get(DemandeTravauxRepository::class);
+        $travaux = $travauxRepository->findOne();
+
+        for($i = 0; $i < count($travaux); $i++) {
+            $client->request('GET', '/plan/demande-travaux/'.$travaux[$i]->getId());
+            $this->assertResponseStatusCodeSame(403, $client->getResponse()->getStatusCode());
+        }
+    }
+
+    public function test_demande_travaux_route_connexion_invalide_usager(): void
+    {
+        $client = static::createClient();
+
+        $travauxRepository = static ::getContainer()->get(DemandeTravauxRepository::class);
+        $travaux = $travauxRepository->findOne();
+
+        for($i = 0; $i < count($travaux); $i++) {
+            $client->request('GET', '/plan/demande-travaux/'.$travaux[$i]->getId());
+            $this->assertResponseStatusCodeSame(403, $client->getResponse()->getStatusCode());
+        }
+    }
+
+    public function test_demande_travaux_nombre_salle_liste_correspond_nombre_salle_bd(){
+
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UtilisateurRepository::class);
+        $testUser = $userRepository->findOneBy(['identifiant' => 'yghamri']);
+
+        // simulate $testUser being logged in
+        $client->loginUser($testUser);
+
+        $systemesAcquisitionRepository = static ::getContainer()->get(SystemeAcquisitionRepository::class);
+        $nbSystemesAcquisitionNonInstalle = count($systemesAcquisitionRepository->findBy(['etat' => 'Non installé']));
+
+        $travauxRepository = static ::getContainer()->get(DemandeTravauxRepository::class);
+        $travaux = $travauxRepository->findOne();
+
+        for($i = 0; $i < count($travaux); $i++) {
+            $crawler = $client->request('GET', '/plan/demande-travaux/'.$travaux[$i]->getId());
+
+            $option = $crawler->filter("#listeSANonInstalle")->filter('option');
+            $this->assertEquals($nbSystemesAcquisitionNonInstalle, $option->count());
+        }
     }
 }
