feedd346fc320ad06db18ed16731336aef28f789
Arnaud Mazurier <amazur01@etudiant.univ-lr.fr>
2024-01-14 21:20:09 +0100

    Ajout de commentaires dans les codes de récupération de la présence et de la qualité de l'air

--- sa-software/software/src/Capteurs/presence.cpp
+++ sa-software/software/src/Capteurs/presence.cpp
@@ -6,12 +6,15 @@
 #define PIN_PRESENCE 4
 
 void initPresence(){
+
     Serial.println("Initialisation de la détection de présence");
 
+    // On met le pin choisi en mode entrée de données
     pinMode(PIN_PRESENCE, INPUT);
 }
 
 bool getPresence()
 {
+    // On renvoie la valeur que le pin indique
     return digitalRead(PIN_PRESENCE);
 }
--- sa-software/software/src/Capteurs/qualAir.cpp
+++ sa-software/software/src/Capteurs/qualAir.cpp
@@ -24,8 +24,11 @@ void array_to_u32(u32 *value, u8* array)
  */
 void u32_to_array(u32 value, u8* array)
 {
+    // Si le tableau n'est pas alloué, ne rien faire
     if(!array)
         return;
+
+    // Sinon, récupérer les valeurs en calculant avec des décalages de bits
     array[0]=value>>24;
     array[1]=value>>16;
     array[2]=value>>8;
@@ -35,34 +38,30 @@ void u32_to_array(u32 value, u8* array)
 /**
  * Fonction permettant de stocker la valeur de la baseline dans l'EEPROM
  */
-void  store_baseline(void)
+void store_baseline(void)
 {
     static u32 i=0;
     u32 j=0;
     u32 iaq_baseline=0;
     u8 value_array[4]={0};
     i++;
-    //Serial.printf("Numéro de rapport : %d\n", i);
     if(i==3600)
     {
         i=0;
-        if(sgp_get_iaq_baseline(&iaq_baseline)!=STATUS_OK)
+        if(sgp_get_iaq_baseline(&iaq_baseline) == STATUS_OK)
         {
-            //Serial.println("get baseline failed!");
-        }
-        else
-        {
-            //Serial.println(iaq_baseline, HEX);
-            //Serial.println("get baseline");
+            // On convertit la valeur de la baseline en 4 valeurs de 8 bits chacune
             u32_to_array(iaq_baseline,value_array);
+
+            // On écrit les 4 valeurs de 8 bits dans l'EEPROM
             for(j=0;j<4;j++){
                 EEPROM.write(j,value_array[j]);
-                //Serial.print(value_array[j]);
-                //Serial.println("...");
             }
             EEPROM.write(j,BASELINE_IS_STORED_FLAG);
         }
     }
+
+    // On attend 1 seconde
     delay(LOOP_TIME_INTERVAL_MS);
 }
 
@@ -77,19 +76,14 @@ void set_baseline(void)
     for(i=0;i<5;i++)
     {
         baseline[i]= EEPROM.read(i);
-        //Serial.print(baseline[i],HEX);
-        //Serial.print("..");
     }
-    //Serial.println("!!!");
     if(baseline[4] != BASELINE_IS_STORED_FLAG)
     {
-        //Serial.println("There is no baseline value in EEPROM");
         return;
     }
-
+    // Convertir les 4 valeurs de 8 bits en une valeur de 32 bits
     array_to_u32(&baseline_value,baseline);
     sgp_set_iaq_baseline(baseline_value);
-    //Serial.println(baseline_value, HEX);
 }
 
 void initQualAir()
@@ -97,13 +91,11 @@ void initQualAir()
     // Initialisation du capteur de qualité de l'air
     s16 err;
     u16 scaled_ethanol_signal, scaled_h2_signal;
-    //Serial.println("//Serial start!!");
 
     /*
      * Vérification du statut du capteur
      */
     while (sgp_probe() != STATUS_OK) {
-        //Serial.println("SGP failed");
         while(1);
     }
 
@@ -112,15 +104,6 @@ void initQualAir()
      */
     err = sgp_measure_signals_blocking_read(&scaled_ethanol_signal,
                                             &scaled_h2_signal);
-
-    /*
-     * Indication de la réussite de la lecture des signaux
-     */
-    if (err == STATUS_OK) {
-        //Serial.println("get ram signal!");
-    } else {
-        //Serial.println("error reading signals");
-    }
     
     // Ecrire la baseline dans l'EEPROM
     set_baseline();
@@ -136,9 +119,6 @@ int getCO2()
     // S'il n'y a pas d'erreur, changer la variable globale et afficher les valeurs de qualité de l'air dans la console
     if (err == STATUS_OK) {
         return (co2_eq_ppm);
-        /*Serial.printf("----- Qualité de l'air ------\n"
-                    "CO2eq Concentration : %03d ppm\n"
-                    "-----------------------------\n", co2_eq_ppm);*/
     } else {
         // Si erreur, afficher un message d'erreur et mettre la variable globale à -1 (valeur d'erreur)
         Serial.println("error reading IAQ values\n");
