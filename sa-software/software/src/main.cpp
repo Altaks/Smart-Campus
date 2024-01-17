6ebf94a6fe321214e1f842f6c3dc80b3a552f588
Arnaud Mazurier <amazur01@etudiant.univ-lr.fr>
2024-01-17 14:45:33 +0100

    Réfactor du main

--- sa-software/software/src/main.cpp
+++ sa-software/software/src/main.cpp
@@ -29,7 +29,7 @@ void setup() {
 
     delay(1000);
     Serial.println("1");
-    // Initilaisation système de fichier
+    // Initialisation système de fichier
     initSystemeFichier();
 
     Serial.println("2");
@@ -44,7 +44,7 @@ void setup() {
     String nomAP = recupererValeur("/infoap.txt","nom_ap");
     String motDePasseAP = recupererValeur("/infoap.txt","mot_de_passe");
     Serial.println("4");
-    // Initialisation reseau en mode STATION et POINT D'ACCES
+    // Initialisation réseau en mode STATION et POINT D'ACCES
     initReseauStationEtPointAcces();
     creerPointAcces(nomAP,motDePasseAP);
     Serial.println("5");
@@ -65,26 +65,26 @@ void setup() {
 
     Serial.println("Connexion au reseau wifi");
 
-    // tant que le SA n'est pas connecté à internet
+    // Tant que le SA n'est pas connecté à internet
     do
     {
-        // enregistre la liste des reseaux dans le fichier listereseaux.txt
+        // Enregistre la liste des réseaux dans le fichier listereseaux.txt
         enregistrerListeReseaux();
 
         nomReseau = recupererValeur("/inforeseau.txt","nom_reseau");
 
-        // si le nom du reseau auquel se connecter est configuré
-        // et que le reseau auquel se connecter est capté par le SA (enregistrer dans le fichier listereseaux.txt)
+        // si le nom du réseau auquel se connecter est configuré
+        // et que le réseau auquel se connecter est capté par le SA (enregistrer dans le fichier listereseaux.txt)
         if (!nomReseau.isEmpty() 
             && estDansFichier("/listereseaux.txt",nomReseau))
         {
-            // recupère les valeurs dans le fichier inforeseau.txt
+            // Récupère les valeurs dans le fichier inforeseau.txt
             int type_eap          = recupererValeur("/inforeseau.txt","type_eap").toInt();
             String password       = recupererValeur("/inforeseau.txt","mot_de_passe");
             String identifiant    = recupererValeur("/inforeseau.txt","identifiant");
             String nomUtilisateur = recupererValeur("/inforeseau.txt","nom_utilisateur");
             
-            // Essaye de se connecter au reseau
+            // Essaie de se connecter au réseau
             if(connexionWifi(nomReseau, wpa2_auth_method_t(type_eap), password ,identifiant, nomUtilisateur))
             {
                 Serial.println("Connexion a "+nomReseau+" Reussie");
@@ -98,10 +98,10 @@ void setup() {
     }
     while(!estConnecte(nomReseau));
     
-    // Initialise l'heure (peut prendre quelques secondes avant de se connecté au serveur ntp)
+    // Initialise l'heure (peut prendre quelques secondes avant de se connecter au serveur ntp)
     initHeure();
-    Serial.print("Initilisation de la date en cours");
-    displayText("Initilisation de la\ndate en cours...");
+    Serial.print("Initialisation de la date en cours");
+    displayText("Initialisation de la\ndate en cours...");
 
     while (!dateEstInitialisee())
     {
@@ -119,14 +119,13 @@ void setup() {
     afficherContenuFichier("/inforeseau.txt");
     afficherContenuFichier("/listereseau.txt");
 
-    // Active l'enregistrement périodique des reseaux wifi détectés par l'esp dans le fichier /listereseaux.txt
+    // Active l'enregistrement périodique des réseaux wifi détectés par l'ESP dans le fichier /listereseaux.txt
     // activerEnregistrerListeReseau();
 
     // Initialise les capteurs 
     initTempEtHum();
     initQualAir();
     initPresence();
-
 
     // Active l'affichage carrousel  
     if (affiche) {initTacheAffichage();}
--- sa-software/software/src/typeDef.h
+++ sa-software/software/src/typeDef.h
@@ -5,18 +5,12 @@
 // valeurs des pins
 const int pinTempEtHum = 17;
 
-// temperature et humidité :
-struct TempEtHum{
-    float temperature;
-    float humidite;
-};
-
-// page courante dans les valeurs (1 -> Température, 2 -> Humidité, 3 -> CO2); // affichage de la luminosité et de la présence nom demandés par le client
+// page courante dans les valeurs (1 -> Température, 2 -> Humidité, 3 -> CO2) ;
+// affichage de la luminosité et de la présence non-demandés par le client
 enum PAGE {
     TEMPERATURE,
     HUMIDITE,
     CO2
 };
-
 
 #endif
